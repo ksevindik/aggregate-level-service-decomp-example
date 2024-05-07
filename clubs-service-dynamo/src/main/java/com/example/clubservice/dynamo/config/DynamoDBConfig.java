05195561a197333ccd1baf8f07d43a1532f3b543
kenan.sevindik <kenan.sevindik@udemy.com>
2024-05-07 19:29:18 +0300

    updates

--- clubs-service-dynamo/src/main/java/com/example/clubservice/dynamo/config/DynamoDBConfig.java
+++ clubs-service-dynamo/src/main/java/com/example/clubservice/dynamo/config/DynamoDBConfig.java
@@ -6,18 +6,23 @@ import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class DynamoDBConfig {
+
+    @Value("${service.aws.service-endpoint}")
+    private String serviceEndpoint;
+
     @Bean
     public AmazonDynamoDB amazonDynamoDB() {
         return AmazonDynamoDBClientBuilder.standard()
                 .withCredentials(new AWSStaticCredentialsProvider(
                         new BasicSessionCredentials("user1","secret1","token1")))
                 .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(
-                        "http://localhost:8000", "us-west-2")) // Modify the region as needed
+                        serviceEndpoint, "us-west-2")) // Modify the region as needed
                 .build();
     }
 
